82bacd0c023c3981f26ad59f19aafd7b5a9d8d1b
Adnaanahma <adnanhashim1212@gamil.com>
2025-01-29 21:24:40 +0100

    check

--- Library.Service/obj/Debug/net8.0/Library.Service.AssemblyInfo.cs
+++ Library.Service/obj/Debug/net8.0/Library.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ced5b308f60a87f5e2e4169d05b8aca4274bce61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc96d7adb59a4e8a669d7f1e237ad9e1f986237")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
